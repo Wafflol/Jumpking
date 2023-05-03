885446d1e92a20886cc7765878da0e2a29b505d9
tommy <tommy.00912@gmail.com>
2023-05-03 10:29:06 +0800

    Initial
    added game files

---
+++ Jump-King-game/ReturnMetadata.js
@@ -0,0 +1,4 @@
+import {returnMeta} from './Player.js';
+//const player = new Player();
+console.log("hello world")
+returnMeta();
---
+++ Jump-King-game/Run.sh
@@ -0,0 +1,1 @@
+php -S localhost:8000 -t /home/wafflol/Downloads/Jump-King-main
